dbae4d1c4b113df15f708ad824fe05bef4d863eb
Jake Carpenter <jake@jakecarpenter.dev>
2021-09-05 00:29:07 -0600

    Convert result with value to IActionResult

---
+++ src/EndpointResult.AspNetCore.Mvc/EndpointResultExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EndpointResult.AspNetCore.Mvc
+{
+    public static class EndpointResultExtensions
+    {
+        public static IActionResult ToActionResult<T>(IEndpointResult<T> endpointResult)
+        {
+            return new ObjectResult(endpointResult.Value) { StatusCode = endpointResult.StatusCode };
+        }
+    }
+}
