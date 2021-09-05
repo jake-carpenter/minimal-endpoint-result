801ca784a3435cce6b44e8586dea800bf4cbb03f
Jake Carpenter <jake@jakecarpenter.dev>
2021-09-05 00:42:08 -0600

    Add None type

---
+++ src/EndpointResult/None.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace EndpointResult
+{
+    public struct None : IEquatable<None>, IComparable<None>, IComparable
+    {
+        public static None Value { get; }
+
+        public override int GetHashCode() => 0;
+        public static bool operator ==(None _, None __) => true;
+        public static bool operator !=(None _, None __) => false;
+        public bool Equals(None _) => true;
+        public override bool Equals(object obj) => obj is None other && Equals(other);
+        public int CompareTo(None _) => 0;
+        public int CompareTo(object _) => 1;
+    }
+}
