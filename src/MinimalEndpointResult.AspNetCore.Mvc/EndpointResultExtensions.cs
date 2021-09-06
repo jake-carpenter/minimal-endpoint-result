4021781af53b6d7a09a21aaef08fe220296e6f13
Jake Carpenter <jake@jakecarpenter.dev>
2021-09-06 12:14:43 -0600

    Add async mappers for MVC

--- src/MinimalEndpointResult.AspNetCore.Mvc/EndpointResultExtensions.cs
+++ src/MinimalEndpointResult.AspNetCore.Mvc/EndpointResultExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MinimalEndpointResult.AspNetCore.Mvc
 {
@@ -23,6 +24,29 @@ namespace MinimalEndpointResult.AspNetCore.Mvc
         public static IActionResult ToActionResult(this IEndpointResult endpointResult)
         {
             return new StatusCodeResult(endpointResult.StatusCode);
+        }
+
+        /// <summary>
+        /// Convert <see cref="IEndpointResult{T}"/> to an <see cref="IActionResult"/> with
+        /// the source HTTP status code and content body.
+        /// </summary>
+        /// <param name="endpointResultTask">Source instance of <see cref="IEndpointResult{T}"/> to convert.</param>
+        /// <typeparam name="T">Typed value to include with the response.</typeparam>
+        public static async Task<IActionResult> ToActionResult<T>(this Task<IEndpointResult<T>> endpointResultTask)
+        {
+            var result = await endpointResultTask;
+            return result.ToActionResult();
+        }
+
+        /// <summary>
+        /// Convert <see cref="IEndpointResult"/> to an <see cref="IActionResult"/> with
+        /// the source HTTP status code.
+        /// </summary>
+        /// <param name="endpointResultTask">Source instance of <see cref="IEndpointResult"/> to convert.</param>
+        public static async Task<IActionResult> ToActionResult(this Task<IEndpointResult> endpointResultTask)
+        {
+            var result = await endpointResultTask;
+            return result.ToActionResult();
         }
     }
 }
